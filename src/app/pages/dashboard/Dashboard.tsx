eee2cf1a96e2f00ce8a826b754f87dea769e00e6
Daniel Calistrato <dancalist.dev@gmail.com>
2023-02-03 15:20:57 -0300

    feat: add login page and form

--- src/app/pages/dashboard/Dashboard.tsx
+++ src/app/pages/dashboard/Dashboard.tsx
@@ -1,3 +1,29 @@
+import { Link } from "react-router-dom";
+
+const Button = () => {
+  return <button>Comprar</button>;
+};
+
+const Button1 = () => {
+  return <button>Comprar1</button>;
+};
+
+const Titulo = () => {
+  return <h1>Meu Título</h1>;
+};
+const Input = () => {
+  return <input type="text" />;
+};
+
 export const Dashboard = () => {
-  return <p>Dashboard</p>;
+  return (
+    <div>
+      <p>Dashboard</p>
+      <Link to="/entrar">Login</Link>
+      <Titulo />
+      <Input />
+      <Button />
+      <Button1 />
+    </div>
+  );
 };
